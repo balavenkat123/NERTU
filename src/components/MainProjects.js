c102901d80628ccfdc9b6d21f119963d720abc33
balavenkat123 <94106481+balavenkat123@users.noreply.github.com>
2023-10-10 04:43:41 +0000

    resolved-seemore-err

--- src/components/MainProjects.js
+++ src/components/MainProjects.js
@@ -5,6 +5,7 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Projectss from './Projectss';
+import { useNavigate } from 'react-router-dom';
 
 const MainProjects = () => {
     const data = [
@@ -69,6 +70,10 @@ const MainProjects = () => {
             "Name of Investigators": "Dr.S.Ananda Mohan"
           }
     ]
+    const Navigate = useNavigate();
+    const seeMore=()=>{
+      Navigate("/projects");
+    } 
   return (
     <div>
         <h1 className='text-start text-danger mx-2 mt-5 px-3'> Main Projects</h1>
@@ -107,7 +112,7 @@ const MainProjects = () => {
             </tbody>
           </Table> </center>     
         {/* </Container> */}
-        <button className='bg-info' onClick={<Projectss/>}>See Moree</button>
+        <button className='bg-info' onClick={seeMore}>See Moree</button>
     </div>
   )
 }
